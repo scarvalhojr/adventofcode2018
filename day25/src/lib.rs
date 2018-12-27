fa281b48f2fee42fe35fc21b8dd72807e93a46ae
Sergio de Carvalho <scarvalhojr@gmail.com>
2018-12-27 18:18:39 +0000

    Day 25

---
+++ day25/src/lib.rs
@@ -0,0 +1,83 @@
+use regex::Regex;
+use std::collections::HashSet;
+use std::num::ParseIntError;
+use std::str::FromStr;
+
+#[derive(Eq, Hash, PartialEq)]
+pub struct Point(i8, i8, i8, i8);
+
+impl Point {
+    fn is_near(&self, other: &Point) -> bool {
+        (self.0 - other.0).abs()
+            + (self.1 - other.1).abs()
+            + (self.2 - other.2).abs()
+            + (self.3 - other.3).abs()
+            <= 3
+    }
+}
+
+struct Constellation<'a> {
+    points: HashSet<&'a Point>,
+}
+
+impl<'a> Constellation<'a> {
+    fn new(point: &'a Point) -> Self {
+        let mut points = HashSet::new();
+        points.insert(point);
+        Constellation { points }
+    }
+
+    fn is_near(&self, other: &Constellation) -> bool {
+        self.points
+            .iter()
+            .any(|point| other.points.iter().any(|p| point.is_near(p)))
+    }
+
+    fn join(&mut self, mut other: Constellation<'a>) {
+        self.points.extend(other.points.drain());
+    }
+}
+
+pub fn part1(points: &[Point]) -> u32 {
+    let mut count = 0;
+    let mut pending: Vec<Constellation> =
+        points.into_iter().map(Constellation::new).collect();
+    while let Some(mut constellation) = pending.pop() {
+        let near_idx = pending
+            .iter()
+            .enumerate()
+            .find(|(_, other)| constellation.is_near(other))
+            .map(|(idx, _)| idx);
+
+        if let Some(idx) = near_idx {
+            let near_const = pending.remove(idx);
+            constellation.join(near_const);
+            pending.push(constellation);
+        } else {
+            count += 1;
+        }
+    }
+
+    count
+}
+
+impl FromStr for Point {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let pattern = Regex::new(r"^(-?\d+),(-?\d+),(-?\d+),(-?\d+)$").unwrap();
+        let groups = pattern.captures(s).ok_or("Invalid format")?;
+        let numbers: Vec<i8> = groups
+            .iter()
+            .skip(1)
+            .map(|val| {
+                val.unwrap()
+                    .as_str()
+                    .parse()
+                    .map_err(|err: ParseIntError| err.to_string())
+            })
+            .collect::<Result<_, _>>()?;
+
+        Ok(Point(numbers[0], numbers[1], numbers[2], numbers[3]))
+    }
+}
---
+++ day25/src/main.rs
@@ -0,0 +1,53 @@
+use clap::{crate_description, App, Arg};
+use day25::{part1, Point};
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error, ErrorKind};
+use std::process::exit;
+
+fn main() {
+    let args = App::new(crate_description!())
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Sets the input file to use")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+    println!(crate_description!());
+    let points = read_input(args.value_of("INPUT").unwrap());
+    println!("Part 1: {}", part1(&points));
+    println!("Part 2: You're done :-)");
+}
+
+fn read_input(filename: &str) -> Vec<Point> {
+    let file = match File::open(filename) {
+        Ok(file) => file,
+        Err(err) => {
+            println!("Failed to open file '{}': {}", filename, err.to_string());
+            exit(2);
+        }
+    };
+
+    match BufReader::new(file)
+        .lines()
+        .map(|line| {
+            line.and_then(|value| {
+                value
+                    .parse()
+                    .map_err(|err| Error::new(ErrorKind::InvalidData, err))
+            })
+        })
+        .collect()
+    {
+        Ok(points) => points,
+        Err(err) => {
+            println!(
+                "Failed to parse input file '{}': {}",
+                filename,
+                err.to_string()
+            );
+            exit(3);
+        }
+    }
+}
